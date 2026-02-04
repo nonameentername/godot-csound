1c6301f1e58a5396ce30c5cf248e8ff2d65b1032
werner mendizabal <nonameentername@gmail.com>
2026-02-03 18:41:05 -0600

    Fix editor csound instances script

--- addons/csound/editor_csound_instances.gd
+++ addons/csound/editor_csound_instances.gd
@@ -72,7 +72,7 @@ func _notification(what):
 
 
 func _update_theme():
-	var stylebox: StyleBoxFlat = get_theme_stylebox("panel", "Tree")
+	var stylebox: StyleBoxEmpty = get_theme_stylebox("panel", "Tree")
 	$CsoundScroll.add_theme_stylebox_override("panel", stylebox)
 
 	for control in $CsoundScroll/CsoundHBox.get_children():
